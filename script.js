7bbeb642c89b06cb8ded0e923797393898a63b72
njf092024 <njf092024@gmail.com>
2024-09-28 18:41:26 +0200

    uwu

---
+++ script.js
@@ -0,0 +1,8 @@
+// HTML button elements
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+
+//button1 click event listener to apply function
+button1.addEventListener("click", function() {
+    //your function code here
+});
